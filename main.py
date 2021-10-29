235384c8a81302f9eeb5b039540729b3eaddad31
Manste <manuelle.ndamtang@yahoo.com>
2021-10-29 13:31:37 +0200

    to fix: display the  icons

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
+import os
+import sys
+
+from ui.ui_interface import *
+from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.show()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    sys.exit(app.exec_())    
